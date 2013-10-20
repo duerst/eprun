2309e3a6e471ef752967c3bbbefa8d2dded6d882
Martin Duerst <duerst@it.aoyama.ac.jp>
2013-10-20 09:18:20 +0900

    removed unused variables

--- benchmark/benchmark.rb
+++ benchmark/benchmark.rb
@@ -29,27 +29,27 @@ def benchmark_test(test_data)
   Benchmark.bm(6) do |x|
     if String.method_defined? :encode
       puts "Fast Normalization using gsub and hash (100 times)"
-      nfd_time = x.report("NFD:")   { 100.times { test_data.text.normalize(:nfd) } }
-      nfkd_time = x.report("NFKD:") { 100.times { test_data.text.normalize(:nfkd) } }
-      nfc_time = x.report("NFC:")   { 100.times { test_data.text.normalize(:nfc) } }
-      nfkc_time = x.report("NFKC:") { 100.times { test_data.text.normalize(:nfkc) } }
+      x.report("NFD:")   { 100.times { test_data.text.normalize(:nfd) } }
+      x.report("NFKD:") { 100.times { test_data.text.normalize(:nfkd) } }
+      x.report("NFC:")   { 100.times { test_data.text.normalize(:nfc) } }
+      x.report("NFKC:") { 100.times { test_data.text.normalize(:nfkc) } }
       puts "Hash size: NFD #{Normalize::NF_HASH_D.size}, NFC #{Normalize::NF_HASH_C.size}, K #{Normalize::NF_HASH_K.size}"
     end
     if self.class.const_defined? :UnicodeUtils
       puts
       puts "Using unicode_utils gem (100 times)"
-      utils_nfd_time = x.report("NFD:")   { 100.times { UnicodeUtils.canonical_decomposition(test_data.text) } }
-      utils_nfkd_time = x.report("NFKD:") { 100.times { UnicodeUtils.nfkd(test_data.text) } }
-      utils_nfc_time = x.report("NFC:")   { 100.times { UnicodeUtils.nfc(test_data.text) } }
-      utils_nfkc_time = x.report("NFKC:") { 100.times { UnicodeUtils.nfkc(test_data.text) } }
+      x.report("NFD:")   { 100.times { UnicodeUtils.canonical_decomposition(test_data.text) } }
+      x.report("NFKD:") { 100.times { UnicodeUtils.nfkd(test_data.text) } }
+      x.report("NFC:")   { 100.times { UnicodeUtils.nfc(test_data.text) } }
+      x.report("NFKC:") { 100.times { UnicodeUtils.nfkc(test_data.text) } }
     end
     if String.method_defined? :localize
       puts
       puts "Using twitter_cldr gem (a single time)"
-      twitter_cldr_nfd_time = x.report("NFD:")   { test_data.text.localize.normalize(:using => :NFD)}
-      twitter_cldr_nfkd_time = x.report("NFKD:") { test_data.text.localize.normalize(:using => :NFKD)}
-      twitter_cldrs_nfc_time = x.report("NFC:")  { test_data.text.localize.normalize(:using => :NFC)}
-      twitter_cldr_nfkc_time = x.report("NFKC:") { test_data.text.localize.normalize(:using => :NFKC)}
+      x.report("NFD:")   { test_data.text.localize.normalize(:using => :NFD)}
+      x.report("NFKD:") { test_data.text.localize.normalize(:using => :NFKD)}
+      x.report("NFC:")  { test_data.text.localize.normalize(:using => :NFC)}
+      x.report("NFKC:") { test_data.text.localize.normalize(:using => :NFKC)}
     end
   end
   puts
